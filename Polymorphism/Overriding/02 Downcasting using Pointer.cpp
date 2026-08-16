ab269b61e2eac85162e17af009f373ab2ae2b96c
Shivani Ashok Mandave <shivanimandave01@gmail.com>
2026-08-16 10:52:13 +0530

    Add files via upload

---
+++ Polymorphism/Overriding/02 Downcasting using Pointer.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class Base
+{
+    public:
+        int N1,N2,Sum;
+
+    Base();
+    ~Base();
+    void Add(int N1,int N2);
+    void Display();
+};
+    Base :: Base()
+    {
+        N1 = 0;
+        N2 = 0;
+        Sum = 0;
+    }
+     void Base :: Add(int N1,int N2)
+    {
+        Sum = N1 + N2;
+        cout<<"Inside Base Class Addition of Two Numbers is"<<Sum<<endl;
+    }
+    void Base :: Display()
+    {
+        cout<<"Inside Base Class Addition of Two Numbers is"<<Sum<<endl;
+    }
+    Base :: ~Base()
+    {
+    }
+class Derived : public Base
+{
+    public:
+        int Sum;
+
+        Derived();
+        ~Derived();
+        void Add (int ,int);
+};
+    Derived :: Derived()
+    {
+        Sum = 0;
+        cout<<"Inside Derived Constructor"<<endl;
+    }
+    void Derived :: Add(int N1,int N2)
+    {
+        Sum = N1 + N2;
+        cout<<"Inside Derived Class Addition of Two Numbers is"<<Sum<<endl;
+    }
+    Derived :: ~Derived()
+    {
+    }
+int main()
+{
+    Base bObj;
+
+    Derived *dPtr = &bObj;//down casting
+
+    dPtr->Add(20,30);
+
+    getch();
+    return 0;
+}
+
